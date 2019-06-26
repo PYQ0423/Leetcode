37a6bcbfbacb86d300ec240b9864150148bc6485
PYQ0423 <403995084@qq.com>
2019-06-26 10:32:04 +0800

    Array section 2 more fixed.

---
+++ src/question/findIntersect.java
@@ -0,0 +1,67 @@
+package question;
+
+import java.util.Arrays;
+
+/**
+ * @program: Leetcode
+ * @author: Programming Queen
+ * @create: 2019-06-26 08:30
+ **/
+
+public class findIntersect {
+        public int[] intersect(int[] nums1, int[] nums2) {
+            int[] result = new int[nums1.length+ nums2.length];
+            int index = 0;
+            // 先排序
+            sort(nums1);
+            sort(nums2);
+
+            // 用两个指针分别遍历A和B
+            // if(nums1[i]==nums2[j])
+            int i=0, j=0;
+            // 边界判断
+            if(nums1.length==0 || nums2.length==0 || nums2[nums2.length-1]<nums1[0] || nums1[nums1.length-1]<nums2[0])
+                return new int[] {};
+            while(i<nums1.length&&j<nums2.length){
+                if(nums1[i]>nums2[j])
+                    // nums1: 1,1,2,2
+                    // nums2: 2,2
+                    j++;
+                else if(nums1[i]<nums2[j])
+                    i++;
+                else{
+                    result[index] = nums1[i];
+                    index++;
+                    i++;
+                    j++;
+                }
+            }
+            int[] result1 = Arrays.copyOf(result,index);
+            return result1;
+        }
+
+        private void sort(int[] nums){
+            for(int i=0;i<nums.length;i++){
+                for(int j=nums.length-1; j>i; j--){
+                    // 只需比较相邻两个数即可
+                    // 从小到大排列
+                    if(nums[j-1]>nums[j])
+                        swap(j-1,j,nums);
+                }
+            }
+        }
+
+        private void swap(int i, int j, int nums[]){
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+
+        public static void main(String[] args){
+            int[] nums1 = {1,2,2,1};
+            int[] nums2 = {2,2};
+            findIntersect fi = new findIntersect();
+            fi.intersect(nums1,nums2);
+        }
+}
+
---
+++ src/question/plusOne.java
@@ -0,0 +1,49 @@
+package question;
+
+import java.util.Arrays;
+
+/**
+ * @program: Leetcode
+ * @author: Programming Queen
+ * @create: 2019-06-26 09:38
+ **/
+
+public class plusOne {
+    // 主要是考虑到可能会进位
+    // 比如 449 + 1 = 450
+    // 499 + 1 = 500
+    // 也就是说最后一位+1，很可能会影响前面的一位，两位，三位
+    // 但是进位只会发生在出现9的时候，出现9，前一位就会+1，该位则变为0
+    // 而且9必须是连续的，才会出现连续进位
+    // 从最后一个数开始看
+    public int[] plusOne(int[] digits) {
+
+        int count = 0;
+        int n = digits.length;
+        // 求出从末尾开始有几个连续的9
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9)
+                break;
+            else
+                count++;
+        }
+
+        if (n == 0)
+            return new int[]{};
+        else if (count == 0 || (n == 1 && count!=1)) {
+            digits[n - 1]++;
+        }
+        else if(count==n){
+            int[] digits1 = new int[n+1];
+            digits1[0] =1;
+            return digits1;
+        }
+        else {
+            for (int i = count; i > 0; i--)
+                digits[n - i] = 0;
+            digits[n - count - 1]++;
+        }
+        return digits;
+    }
+}
+
